fa7533e2d2c0e317f6dcdde20c72224b82b27f1e
Jayesh09871 <jayeshkakhani29@gmail.com>
2024-10-22 17:28:00 +0530

    updated code

---
+++ server/src/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const route = require("./routes/route");
+const { default: mongoose } = require("mongoose");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.URL, { useNewUrlParser: true })
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(process.env.PORT, function () {
+  console.log("Express app running on PORT : " + process.env.PORT);
+});
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const route = require("./routes/route");
+// const { default: mongoose } = require("mongoose");
+// const app = express();
+// require("dotenv").config();
+
+// app.use(express.json());
+
+// mongoose.set("strictQuery", false);
+
+// mongoose
+//   .connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true }) 
+//   .then(() => console.log("MongoDb is connected"))
+//   .catch((err) => console.log(err));
+
+// app.use("/", route);
+
+// app.listen(process.env.PORT, function () {
+//   console.log("Express app running on port " + process.env.PORT);
+// });
